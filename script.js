ca311a330a62a0ccad829abca45271d9082b5e98
NJWeb.Solutions <dibend8@gmail.com>
2024-09-16 10:13:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Current slide controls
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-// Show the slide based on the current index
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
@@ -20,15 +19,17 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
 
-  // Hide all slides and deactivate all dots
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Deactivate all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  // Show the current slide and highlight the active dot
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  // Show the current slide and activate the dot
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
